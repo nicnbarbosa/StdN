e1cae4767f30de3039dd07e375bd6408fc929b57
Aluno <aluno@ifslab.edu.br>
2019-06-12 16:38:47 -0200

    Classe Cliente Incompleta

--- projeto/StudioN/Cliente.java
+++ projeto/StudioN/Cliente.java
@@ -2,4 +2,43 @@ package StudioN;
 
 public class Cliente extends Pessoa {
 
+	private String CodCliente;
+	private String QuantFotos;
+	private String Horas;
+	
+	public Cliente () {
+		super();
+	}
+	
+	public Cliente (String codCliente, String qFotos, String horas) {
+		this.setCodCliente(codCliente);
+		this.setQuantFotos(qFotos);
+		this.setHoras(horas);
+	}
+
+	public String getCodCliente() {
+		return CodCliente;
+	}
+
+	public void setCodCliente(String codCliente) {
+		CodCliente = codCliente;
+	}
+
+	public String getQuantFotos() {
+		return QuantFotos;
+	}
+
+	public void setQuantFotos(String quantFotos) {
+		QuantFotos = quantFotos;
+	}
+
+	public String getHoras() {
+		return Horas;
+	}
+
+	public void setHoras(String horas) {
+		Horas = horas;
+	}
+	
+	
 }
